bd01005c0049e8cc46e7ad8f64077826e3e2d5f0
olga-maev <kurumoch0@gmail.com>
2022-05-24 14:06:29 +0400

    Grammatic error off

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) +" миллилитров воды");
         System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(он) бёдер(ро)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
     }
 }
